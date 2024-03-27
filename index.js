bab83931580fc78fecc33cc0daa6a918e140597f
Kelvin Machira <machirakelvin81@gmail.com>
2024-03-27 10:17:32 +0300

    Arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+//Multiplication whose result - 62
+//1* 62
+//2*31
+let num1 = 2; 
+let num2 = 31;
+const multiply = num1*num2 ;
+
+let random = Math.ceil(Math.random() * 10) ; 
+
+let num3 = 11;
+let num4 = 7;
+
+let mod = num3 % num4 ;
+
+let max = Math.max(20);
+Math.max(4,5,1,10,20,13,6);
+console.log(max);
+
+    
